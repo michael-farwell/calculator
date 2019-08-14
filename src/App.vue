2e041e6b60ee6d42c23ad4aeaeb77f88dc3aa2e9
Michael Farwell <whitewolf9909@gmail.com>
2019-08-14 15:06:17 -0400

    Start calculator

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,23 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <b-container>
+            <b-jumbotron header="Calculator">
+                <Calculator/>
+            </b-jumbotron>
+        </b-container>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import Calculator from './components/Calculator.vue';
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+  export default {
+    name: 'app',
+    components: {
+      Calculator,
+    },
+  };
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ src/components/Calculator.vue
@@ -0,0 +1,96 @@
+<template>
+    <b-row class="justify-content-center">
+        <b-col sm="6">
+            <b-card title="Current Value" class="text-right">
+                <b-card-body>
+                    <b-row class="mt-3">
+                        <b-col sm="3">
+                            <b-button>AC</b-button>
+                        </b-col>
+                        <b-col sm="3">
+                            <b-button>C</b-button>
+                        </b-col>
+                        <b-col sm="3">
+                            <b-button>+/-</b-button>
+                        </b-col>
+                        <b-col sm="3">
+                        </b-col>
+                    </b-row>
+                    <b-row class="mt-3">
+                        <b-col sm="3">
+                            <b-button>7</b-button>
+                        </b-col>
+                        <b-col sm="3">
+                            <b-button>8</b-button>
+                        </b-col>
+                        <b-col sm="3">
+                            <b-button>9</b-button>
+                        </b-col>
+                        <b-col sm="3">
+                            <b-button>+</b-button>
+                        </b-col>
+                    </b-row>
+                    <b-row class="mt-3">
+                        <b-col sm="3">
+                            <b-button>4</b-button>
+                        </b-col>
+                        <b-col sm="3">
+                            <b-button>5</b-button>
+                        </b-col>
+                        <b-col sm="3">
+                            <b-button>6</b-button>
+                        </b-col>
+                        <b-col sm="3">
+                            <b-button>-</b-button>
+                        </b-col>
+                    </b-row>
+                    <b-row class="mt-3">
+                        <b-col sm="3">
+                            <b-button>1</b-button>
+                        </b-col>
+                        <b-col sm="3">
+                            <b-button>2</b-button>
+                        </b-col>
+                        <b-col sm="3">
+                            <b-button>3</b-button>
+                        </b-col>
+                        <b-col sm="3">
+                            <b-button>X</b-button>
+                        </b-col>
+                    </b-row>
+                    <b-row class="mt-3">
+                        <b-col sm="3">
+                            <b-button>0</b-button>
+                        </b-col>
+                        <b-col sm="3">
+                            <b-button>.</b-button>
+                        </b-col>
+                        <b-col sm="3">
+                            <b-button>=</b-button>
+                        </b-col>
+                        <b-col sm="3">
+                            <b-button>/</b-button>
+                        </b-col>
+                    </b-row>
+                </b-card-body>
+            </b-card>
+        </b-col>
+    </b-row>
+</template>
+
+<script>
+  export default {};
+</script>
+
+<style scoped>
+    button {
+        width: 100px;
+        height: 100px;
+        font-size: 24pt;
+        margin: 0;
+    }
+
+    button.zero {
+        width: 200px;
+    }
+</style>
